f170a4fc53728dbf6344d071e8b5aace3e5fb1ec
vicent.boned <vicent.boned@supercell.com>
2023-08-10 11:34:34 +0300

    Update directory

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir ./secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret my_secret.gpg
+--output ./secrets/my_secret my_secret.gpg
